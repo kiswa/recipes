a9eaefef7d5a082b34446a9e9e767dba47688b52
Matthew Ross <kiswa.com@gmail.com>
2017-11-27 20:00:28 -0500

    Switch to Vue.js

--- src/app/models/ingredient.ts
+++
@@ -1,7 +0,0 @@
-export class Ingredient {
-    constructor(public id: number = 0,
-            public name: string = '',
-            public amount: number = 1,
-            public measure: string = '') {
-    }
-}
